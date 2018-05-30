09da294cd8b001824bf6c25eb3a04f9bc912a6b7
Максим Казанцев <maks.aaron@gmail.com>
2018-05-30 13:03:11 +0400

    add Aplication manager class

--- fixture.py
+++ fixture.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
+from model.application import Application
 import pytest
 
 @pytest.fixture
-def driver(request):
+def app(request):
     driver = webdriver.Firefox()
     driver.implicitly_wait(30)
     request.addfinalizer(driver.quit)
-    return driver
+    return Application(driver)
